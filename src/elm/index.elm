c4b2685ad8660321ce5e40ca5b98d8d959dda826
Gulshan Singh <gulshan@umich.edu>
2015-01-01 03:13:01 -0500

    Created custom checkboxes and styled page

--- src/elm/index.elm
+++ src/elm/index.elm
@@ -1,4 +1,5 @@
 import Color (..)
+import Debug
 import Markdown
 import Dict
 import Maybe
@@ -23,8 +24,15 @@ zip listX listY =
     (  _  ,   _  ) -> []
 
 
+port title : String
+port title = "Course Load Calculator"
+
 headerHeight : Int
 headerHeight = 100
+
+color1 = rgb 27 124 192
+color2 = rgb 0 0 0xcd
+color3 = rgb 230 238 255
 
 allCourses : List (String, Int)
 allCourses = [("EECS 280", 3), ("EECS 281", 5), ("EECS 482", 5),
@@ -34,11 +42,12 @@ makeDict : Dict.Dict String (Int, Bool)
 makeDict = List.foldl (\(k, v) d -> Dict.insert k (v, False) d)
            Dict.empty allCourses
 
-updateState : (String, Bool) -> State -> State
-updateState (s, b) state =
-  let newCourseInfo = Dict.update s (\(Just (credits, _)) -> Just (credits, b))
+updateState : String -> State -> State
+updateState s state =
+  let newCourseInfo = Dict.update s (\(Just (credits, b)) -> Just (credits, not b))
                       state.courseInfo
-  in case b of
+      (Just (_, b)) = Dict.get s state.courseInfo
+  in case not b of
       True -> { courseInfo = newCourseInfo,
                 selectedCourses = Set.insert s state.selectedCourses}
       False -> { courseInfo = newCourseInfo,
@@ -46,7 +55,7 @@ updateState (s, b) state =
 
 getState : Signal State
 getState = foldp updateState { courseInfo = makeDict,
-                               selectedCourses = Set.empty} (subscribe check)
+                               selectedCourses = Set.empty} (subscribe click)
 
 box =
   let dim = 200
@@ -63,7 +72,7 @@ view (w, h) s = flow down [header w headerHeight,
                            mainContainer w h s]
 
 mainContainer : Int -> Int -> State -> Element
-mainContainer w h s = color grey (container w (h - headerHeight) middle
+mainContainer w h s = color color3 (container w (h - headerHeight) middle
                                   <| appContainer s 200 500)
 
 appContainer : State -> Int -> Int -> Element
@@ -77,27 +86,28 @@ appContainer s w h =
 
 checkboxContainer : Dict.Dict String (Int, Bool) -> Int -> Int -> Element
 checkboxContainer courseInfo w h =
-  let courses = (zip (snd <| List.unzip <| Dict.values courseInfo)
-               (Dict.keys courseInfo))
-  in color darkGrey <| container w h midTop
-     <|flow down
-     <| List.map (uncurry createCheckbox) courses
+    let courses = zip (snd <| List.unzip <| Dict.values courseInfo)
+                  (Dict.keys courseInfo)
+    in color color1 <| container w h midTop
+           <| flow down
+           <| [spacer 1 2] ++ (List.intersperse (spacer 1 2)
+                               <| List.map (uncurry <| makeButton (color3, color1) (color2, color3)) courses) ++ [spacer 1 2]
 
 selectedCoursesContainer : Set.Set String -> Int -> Int -> Element
 selectedCoursesContainer courses w h =
-  let selectedCourses = (List.map (plainText >> container 100 30 middle)
+  let selectedCourses = (List.map (fromString >> Text.typeface ["Roboto", "sans-serif"] >> Text.color color3 >> centered >> container 100 30 middle)
                          (Set.toList courses))
       foo = [box]
-  in color darkGrey <| container w h midTop <| flow down selectedCourses
+  in color color1 <| container w h midTop <| flow down selectedCourses
 
 resultsContainer : State -> Int -> Int -> Element
 resultsContainer state w h =
   let (Just sum) = Set.foldl (\s (Just acc) -> Maybe.map (\(i, _) -> i + acc)
                                         (Dict.get s state.courseInfo))
                    (Just 0) state.selectedCourses
-  in color darkGrey <| container w h midTop <| flow down [
-    plainText <| "Workload score: " ++ toString sum,
-    Markdown.toElement <| "Semester rating: " ++ resultsMessage sum]
+  in color color1 <| container w h midTop <| flow down [
+          centered <| Text.color color3 <| fromString <| "Workload score: " ++ toString sum,
+                   Markdown.toElement <| "Semester rating: " ++ resultsMessage sum]
 
 resultsMessage : Int -> String
 resultsMessage sum = if | sum <= 5 -> "Easy"
@@ -106,10 +116,10 @@ resultsMessage sum = if | sum <= 5 -> "Easy"
                         | sum > 15 -> "It's going to be a tough semester..."
 
 header : Int -> Int -> Element
-header w h = color darkGrey (container w h middle titleText)
+header w h = color color1 <| container w h middle titleText
 
 titleText : Element
-titleText = centered (bold (height 40 (fromString "Class Load Calculator")))
+titleText = Text.color color3 >> height 40 >> bold >> centered <| fromString "Class Load Calculator"
 
 check : Channel (String, Bool)
 check = channel ("", False)
@@ -118,4 +128,13 @@ createCheckbox : Bool -> String -> Element
 createCheckbox checked label =
   flow right [container 30 30 middle
               (Input.checkbox (\b -> send check (label, b)) checked),
-              container 100 30 middle (plainText label)]
+              container 100 30 middle <| centered <| Text.color color3 <| fromString label]
+
+click : Channel String
+click = channel ""
+
+makeButton : (Color, Color) -> (Color, Color) -> Bool -> String -> Element
+makeButton upColor downColor b label =
+    let button bColor tColor = color bColor <| container 196 40 middle <| centered <| typeface ["Roboto", "sans-serif"] <| Text.color tColor <| fromString label
+        (backgroundColor, textColor) = if b then downColor else upColor
+    in Input.clickable (send click label) <| button backgroundColor textColor
