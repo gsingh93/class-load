8a6e03a074b52653a7714942df7efe9a5a7c152f
Gulshan Singh <gulshan@umich.edu>
2015-01-27 16:10:21 -0500

    Added reset.css

--- Foundation.hs
+++ Foundation.hs
@@ -71,7 +71,7 @@ instance Yesod App where
         -- you to use normal widget features in default-layout.
 
         pc <- widgetToPageContent $ do
---            addStylesheet $ StaticR css_bootstrap_css
+            addStylesheet $ StaticR css_reset_css
             $(widgetFile "default-layout")
         withUrlRenderer $(hamletFile "templates/default-layout-wrapper.hamlet")
 
